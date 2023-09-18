9d91ba3038b29b6ffdd84887d0c7511b581a68c8
LordSerg <oknelaksoms@gmail.com>
2023-09-18 03:04:12 +0300

    third commit

---
+++ prison_experiment/prison_experiment/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace prison_experiment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(524, 14);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 77);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "start experiment";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(505, 612);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Location = new System.Drawing.Point(525, 119);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(160, 26);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(523, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Number of prisoners:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(523, 292);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Number of experiments:";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown2.Location = new System.Drawing.Point(525, 315);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(160, 26);
+            this.numericUpDown2.TabIndex = 5;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Location = new System.Drawing.Point(523, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 43);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Number of tries for each prisoner:";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown3.Location = new System.Drawing.Point(525, 213);
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(160, 26);
+            this.numericUpDown3.TabIndex = 7;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(570, 604);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(78, 20);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "The riddle";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 636);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ prison_experiment/prison_experiment/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prison_experiment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int priz_num=100;//кількість в'язнів = кількість коробок
+        int check_num=50;//кількість разів для перевірки
+        int TIMES = 50000;//кількість експериментів
+        int[] arr;
+        Random r = new Random();
+        int success_rnd = 0;
+        int success_clvr = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            priz_num = (int)numericUpDown1.Value;
+            check_num = (int)numericUpDown3.Value;
+            TIMES = (int)numericUpDown2.Value;
+            string s = "";
+            success_clvr = 0;
+            success_rnd = 0;
+            for (int i = 1; i <= TIMES; i++)
+            {
+                init_boxes();
+                success_rnd += do_rand_experement() ? 1 : 0;
+                success_clvr += do_clever_experement() ? 1 : 0;
+                s = "experement "+i.ToString() + "/" + TIMES.ToString()+":\n" +
+                    "RANDOM:\n" +
+                    "success = " + success_rnd + "\n" +
+                    "failure = " + (i - success_rnd) + "\n" +
+                    "probability = "+(Math.Round((double)success_rnd/(double)i,10))+"\n\n" +
+                    "ALGORYTHM:\n" +
+                    "success = " + success_clvr + "\n" +
+                    "failure = " + (i - success_clvr) +"\n"+
+                    "probability = " + (Math.Round((double)success_clvr / (double)i, 10));
+                richTextBox1.Text = s;
+                richTextBox1.Refresh();
+            }
+            //string array = "\n";
+            //for (int i = 0; i < priz_num; i++) array+="a["+i+"] = "+arr[i].ToString()+"\n";
+            //s += array;
+            //s += find_circles();
+            richTextBox1.Text = s;
+        }
+        void swap(ref int a, ref int b)
+        {
+            int t = a;
+            a = b;
+            b = t;
+        }
+        void init_boxes()
+        {
+            arr = new int[priz_num];
+            for (int i = 0; i < priz_num; i++) arr[i] = i;
+            for (int i = 0; i < priz_num*50; i++) swap(ref arr[r.Next(priz_num)], ref arr[r.Next(priz_num)]);
+        }
+        bool do_rand_experement() 
+        {//returns, if experement gone well for prizoners
+            int k = 0;
+            List<int> ch = new List<int>();
+            bool is_managed = false;
+            for (int i = 0; i < priz_num; i++)//prisoners
+            {
+                is_managed = false;
+                for (int j = 0; j < check_num; j++)//boxes
+                {
+                    k = r.Next(priz_num);
+                    while (ch.Contains(k))
+                    {
+                        k = r.Next(priz_num);
+                    }
+                    
+                    if (arr[k] == i)//якщо номер в'язня(і) співпадає з випадковим номером, що у коробці(arr[k])
+                    {//то в'язню пощастило
+                        is_managed = true;
+                        break;
+                    }
+                    ch.Add(k);
+                }
+                if (!is_managed)
+                    return false;
+                ch.Clear();
+            }
+            return true;
+        }
+        bool do_clever_experement()
+        {//returns, if experement gone well for prizoners
+            int k = -1;
+            bool is_managed = false;
+            for (int i = 0; i < priz_num; i++)//prisoners
+            {
+                is_managed = false;
+                k = i;
+                for (int j = 0; j < check_num; j++)//boxes
+                {
+                    if (arr[k] == i)//якщо номер в'язня(і) співпадає з номером, що у коробці(arr[k])
+                    {
+                        is_managed = true;
+                        break;
+                    }
+                    k = arr[k];
+                }
+                if (!is_managed)
+                    return false;
+            }
+            return true;
+        }
+        string find_circles()
+        {
+            string answer = "";
+            bool[] flg = new bool[priz_num];
+            for (int t = 0; t < priz_num; t++) flg[t] = true;
+            List<int> cycle = new List<int>();
+            int i = 0,count = 0;
+            int k = 0;
+            do
+            {
+                if (!flg[k])
+                {
+                    answer += "cycle #" + i + ": (length = "+cycle.Count+")\n";
+                    foreach (int n in cycle) answer += "" + n + "->";
+                    answer += cycle[0]+"\n";
+                    cycle.Clear();
+                    k = -1;
+                    for (int t = 0; t < priz_num; t++)
+                        if (flg[t])
+                        {
+                            k = t;
+                            break;
+                        }
+                    i++;
+                }
+                if (k == -1)
+                    break;
+                cycle.Add(k);
+                flg[k] = false;
+                k = arr[k];
+                count++;
+            }
+            while (true);//count < priz_num);
+            return answer;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            numericUpDown3.Maximum = numericUpDown1.Value;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://en.wikipedia.org/wiki/100_prisoners_problem");
+        }
+    }
+}
